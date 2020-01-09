88168b6c0a3cf5e2f125d01bed7f0469571d6d16
RevyLAD <hsm0219@gmail.com>
2020-01-09 18:10:19 +0900

    2020-01-09

--- Team5-SmartMOM/MainForm.cs
+++ Team5-SmartMOM/MainForm.cs
@@ -13,7 +13,6 @@ namespace Team5_SmartMOM
 {
     public partial class MainForm : Form
     {
-        private readonly DrawItemEventHandler tabControl1_DrawItem;
         private Point _imageLocation = new Point(13, 5);
         private Point _imgHitArea = new Point(13, 2);
 
@@ -26,14 +25,6 @@ namespace Team5_SmartMOM
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.menuStrip1.BackColor = Color.FromArgb(255, 255, 230);
-
-            tabControl1.DrawMode = TabDrawMode.OwnerDrawFixed;
-            tabControl1.DrawItem += tabControl1_DrawItem;
-            CloseImage = Properties.Resources.delete;
-            tabControl1.Padding = new Point(10, 3);
-
-
 
         }
         private void Form_Gradient(object sender, PaintEventArgs e)
@@ -165,52 +156,7 @@ namespace Team5_SmartMOM
 
         #endregion
 
-        #region 탭페이지 이미지, x버튼
-        private void metroTabControl1_DrawItem(object sender, DrawItemEventArgs e)
-        {
-            try
-            {
-                Image img = new Bitmap(CloseImage);
-                Rectangle r = e.Bounds;
-                r = this.tabControl1.GetTabRect(e.Index);
-                r.Offset(2, 2);
-                Brush TitleBrush = new SolidBrush(Color.Black);
-                Font f = this.Font;
-                string title = this.tabControl1.TabPages[e.Index].Text;
-
-                e.Graphics.DrawString(title, f, TitleBrush, new PointF(r.X, r.Y));
-
-                if (tabControl1.SelectedIndex >= 0)
-                {
-                    e.Graphics.DrawImage(img, new Point(r.X + (this.tabControl1.GetTabRect(e.Index).Width - _imageLocation.X)-7, _imageLocation.Y));
-                }
-            }
-            catch (Exception) { }
-        }
-
-        private void metroTabControl1_MouseClick(object sender, MouseEventArgs e)
-        {
-            TabControl tc = (TabControl)sender;
-            Point p = e.Location;
-            int _tabWidth = 0;
-            _tabWidth = this.tabControl1.GetTabRect(tc.SelectedIndex).Width - (_imgHitArea.X);
-            Rectangle r = this.tabControl1.GetTabRect(tc.SelectedIndex);
-            r.Offset(_tabWidth, _imgHitArea.Y);
-            r.Width = 16;
-            r.Height = 16;
-            if (tabControl1.SelectedIndex >= 0)
-            {
-                if (r.Contains(p))
-                {
-                    TabPage TabP = (TabPage)tc.TabPages[tc.SelectedIndex];
-                    tc.TabPages.Remove(TabP);
-                }
-
-            }
-        }
-
-        #endregion
-
+        
         private void CreateTabPages(string text, Form OpenForm)
         {
             foreach (TabPage childForm in this.tabControl1.TabPages)
