d11da1db9a83eaddd96fad610351568b62bb8612
RevyLAD <hsm0219@gmail.com>
2020-02-03 18:08:07 +0900

    edsdasd

--- Team5-Pop/POPGaDong.cs
+++ Team5-Pop/POPGaDong.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Timers;
+using log4net.Core;
+using System.IO;
+using System.Configuration;
 
 namespace Team5_Pop
 {
     public partial class POPGaDong : Form
     {
+
+        static System.Timers.Timer timer1;
+        static int machineID = 1;
+        static string writeFolder = AppDomain.CurrentDomain.BaseDirectory + "\\Production";
+        static int iCnt = 0;
         public POPGaDong()
         {
             InitializeComponent();
+        }
+
+        private void POPGaDong_Load(object sender, EventArgs e)
+        {
+            
+            Random rnd = new Random((int)DateTime.UtcNow.Ticks);
+            machineID = rnd.Next(1, 10);
+
+            Console.WriteLine("생산량 전송 프로그램 시작");
+            SetTimer();
+
+            Console.ReadLine();
+            Console.WriteLine("생산량 전송 프로그램 종료");
+        }
+
+        private static void SetTimer()
+        {
+            timer1 = new System.Timers.Timer(3000);
+            timer1.Enabled = true;
+            timer1.Elapsed += timer1_Elapsed;
+            timer1.AutoReset = true;
+        }
+
+        private static void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Random rnd = new Random((int)DateTime.UtcNow.Ticks);
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter($"{writeFolder}\\WorkerLogMachine_{machineID}_{iCnt}.log", false);
+
+                string msg = $"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss")} Machine/{machineID}/{rnd.Next(1, 77)}/{rnd.Next(100, 130)}/{rnd.Next(0, 5)}";
+                sw.WriteLine(msg);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+            finally
+            {
+                iCnt++;
+            }
+        }
+
+        private void POPGaDong_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+            timer1.Stop();
+            timer1.Dispose();
         }
     }
 }
---
+++ Team5-Pop/ProductionService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Team5_Pop
+{
+    public partial class ProductionService : ServiceBase
+    {
+        string strConn = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+
+        public ProductionService()
+        {
+            InitializeComponent();
+        }
+
+        //protected override void OnStart(string[] args)
+        public void OnStart()
+        {
+            Console.WriteLine("서비스 시작");
+            AsyncEchoServer().Wait();
+        }
+
+        async Task AsyncEchoServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+            while (true)
+            { 
+                TcpClient tc = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+                await Task.Factory.StartNew(AsyncTcpProcess, tc);
+            }
+        }
+
+        private async void AsyncTcpProcess(object o)
+        {
+            TcpClient tc = (TcpClient)o;
+            NetworkStream stream = tc.GetStream();
+
+            byte[] buff = new byte[1024];
+            var readTask = stream.ReadAsync(buff, 0, buff.Length);
+            int nbytes = readTask.Result;
+            if (nbytes > 0)
+            {
+                //20200204 12:01:20 Machine7/65/4/0
+                string[] arrData = Encoding.ASCII.GetString(buff, 0, nbytes).Split('/');
+                if (arrData.Length == 5)
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = new SqlConnection(strConn);
+                        cmd.CommandText = "insert into WorkQtyLog(ProductID, MachineID, Qty, BadQty) values (@ProductID, @MachineID, @Qty, @BadQty)";
+
+                        cmd.Parameters.AddWithValue("@ProductID", int.Parse(arrData[2]));
+                        cmd.Parameters.AddWithValue("@MachineID", int.Parse(arrData[1]));
+                        cmd.Parameters.AddWithValue("@Qty", int.Parse(arrData[3]));
+                        cmd.Parameters.AddWithValue("@BadQty", int.Parse(arrData[4]));
+
+                        cmd.Connection.Open();
+                        cmd.ExecuteNonQuery();
+                        cmd.Connection.Close();
+                    }
+                }
+
+
+
+                Console.WriteLine(Encoding.ASCII.GetString(buff, 0, nbytes));
+                Program.Log.WriteInfo(Encoding.ASCII.GetString(buff, 0, nbytes));
+
+                await stream.WriteAsync(buff, 0, nbytes).ConfigureAwait(false);
+            }
+
+            stream.Close();
+            tc.Close();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
