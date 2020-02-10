a12d941199939a3b2261826fd8825f678ad2bc11
tnalsok123 <tnalsok123@naver.com>
2020-02-10 17:57:36 +0900

    ㅄㅁ

--- Team5-SmartMOM/PSM/Stock_State.cs
+++ Team5-SmartMOM/PSM/Stock_State.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using Project_VO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Team5_SmartMOM.Service;
 
 namespace Team5_SmartMOM.PSM
 {
     public partial class Stock_State : Team5_SmartMOM.BaseGridForm
     {
+        CheckBox headerCheckBox = new CheckBox();
+        List<Stock_StateVO> list = new List<Stock_StateVO>();
         public Stock_State()
         {
             InitializeComponent();
@@ -17,16 +21,53 @@ namespace Team5_SmartMOM.PSM
 
         private void Stock_State_Load(object sender, EventArgs e)
         {
-            dataGridView1.Columns.Add("창고코드", "창고코드");
-            dataGridView1.Columns.Add("창고", "창고");
-            dataGridView1.Columns.Add("품목", "품목");
-            dataGridView1.Columns.Add("품명", "품명");
-            dataGridView1.Columns.Add("품목타입", "품목타입");
-            dataGridView1.Columns.Add("규격", "규격");
-            dataGridView1.Columns.Add("재고량", "재고량");
-            dataGridView1.Columns.Add("단위", "단위");
-            dataGridView1.Columns.Add("관리등급", "관리등급");
-            dataGridView1.Columns.Add("비고", "비고");
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+            chk.HeaderText = "";
+            chk.Name = "Check";
+            chk.Width = 30;
+            dataGridView1.Columns.Add(chk);
+
+            Point headerLocation = dataGridView1.GetCellDisplayRectangle(0, -1, true).Location;
+            headerCheckBox.Location = new Point(headerLocation.X + 8, headerLocation.Y + 6);
+            headerCheckBox.BackColor = Color.White;
+            headerCheckBox.Size = new Size(18, 18);
+            headerCheckBox.Click += new EventHandler(HeaderCheckBox_Click);
+            dataGridView1.Controls.Add(headerCheckBox);
+
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "창고코드", "FACT_Code", true, 90);
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "창고", "FACT_Name", true, 90);
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "품목", "ITEM_Code", true, 100);
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "품명", "ITEM_Name", true, 150);
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "품목타입", "ITEM_Type", true, 150);
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "규격", "ITEM_Size", true, 150);
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "재고량", "FACD_Qty", true, 150);            
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "단위", "ITEM_Unit", true, 150);
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "관리등급", "", true, 150);
+            UtilityClass.AddNewColumnToDataGridView(dataGridView1, "비고", "", true, 150);
+
+
+            DataLoad();
+        }
+
+        public void DataLoad()
+        {
+            PSM_Service service = new PSM_Service();
+            list = service.Stock_State();
+            dataGridView1.DataSource = list;
+        }
+
+            private void HeaderCheckBox_Click(object sender, EventArgs e)
+        {
+            dataGridView1.EndEdit();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                DataGridViewCheckBoxCell chkBox = row.Cells["Check"] as DataGridViewCheckBoxCell;
+                chkBox.Value = headerCheckBox.Checked;
+            }
         }
     }
 }
---
+++ Team5-SmartMOM/PSM/XtraReport1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Team5_SmartMOM.PSM
+{
+    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public XtraReport1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
